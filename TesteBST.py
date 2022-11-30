18424f3f03437fde2c881a0d9604cd394f5802f9
pedropeglow <pedropeglowm@gmail.com>
2022-11-30 10:23:11 -0300

    :white_check_mark: Ajuste nos testes.

--- TesteBST.py
+++ TesteBST.py
@@ -4,37 +4,64 @@ from BinarySearchTree import BinarySearchTree
 
 class TestMainTest(unittest.TestCase):
 
-    def remove_element(self, label):
+    def insert_elemento_show_tree(self, label):
+        print("Árvore antes da inserção -> ")
+        self.tree.showTreePre(self.tree.getRoot(), self.tree.getRoot())
+        print("-"*20)
+        print("Elemento a ser inserido: ", label)
+        self.tree.insert(label)
+        print("-"*20)
+        print("Árvore depois da inserção -> ")
+        self.tree.showTreePre(self.tree.getRoot(), self.tree.getRoot())
+
+    def remove_elemento_show_tree_pre(self, label):
+        print("Árvore antes da remocao ->")
+        self.tree.showTreePre(self.tree.getRoot(), self.tree.getRoot())
+        print("-"*20)
+        print("Elemento a ser removido:", label)
+        self.tree.remove(label)
+        print("-"*20)
+        print("Arvore depois da remocao")
+        self.tree.showTreePre(self.tree.getRoot(), self.tree.getRoot())
+
+    def remove_elemento_show_tree_pos(self, label):
         print("Arvore antes da remocao")
-        self.tree.showTree(self.tree.getRoot(), self.tree.getRoot())
+        self.tree.showTreePos()
         print("")
         print("Elemento a ser removido:", label)
-        self.tree.removeNovo(label)
+        self.tree.remove(label)
         print("")
         print("Arvore depois da remocao")
-        self.tree.showTree(self.tree.getRoot(), self.tree.getRoot())
+        self.tree.showTreePos()
 
     def setUp(self):
 
         self.show = True
+        values = [10, 5, 3, 2, 7, 6, 30, 25, 28, 27, 29, 50, 55, 60]
         self.tree = BinarySearchTree()
+        for v in values:
+            self.tree.insert(v)
+        return self.tree
 
-        self.tree.insert(9)
-        self.tree.insert(3)
-        self.tree.insert(2)
-        self.tree.insert(1)
-        self.tree.insert(4)
-        self.tree.insert(5)
-        self.tree.insert(21)
-        self.tree.insert(25)
-        self.tree.insert(29)
-        self.tree.insert(23)
-        self.tree.insert(24)
-        self.tree.insert(31)
 
-    def test_remove_nodo_folha(self):
+    def test_remove_elemento_pos_ordem(self):
+
         if self.show:
-            self.remove_element(31)
+            self.remove_elemento_show_tree_pos(27)
+        self.assertTrue(True)
+
+
+    def test_remove_elemento_pre_ordem(self):
+
+        if self.show:
+            self.remove_elemento_show_tree_pre(6)
+        self.assertTrue(True)
+
+
+
+    def test_insert_element(self):
+        if self.show:
+            self.insert_elemento_show_tree(20)
         self.assertTrue(True)
 
 
