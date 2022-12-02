860ba27176c315cd33c3bd1e5b5bca4ba77dbcc6
pedropeglow <pedropeglowm@gmail.com>
2022-12-02 15:49:43 -0300

    :zap: Ajustando menu.

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ def menu():
         [0] - Close Program
         [1] - Insert an element in Binary Tree
         [2] - Remove an element in Binary Tree
-        [3] - Show Tree Pre
-        [4] - Show Tree In Order
-        [5] - Show Tree Pos
-        [6] - Search an element in Binary Tree
-        [7] - Tree Height
+        [3] - Tree Traversals Preorder
+        [4] - Tree Traversals Inorder
+        [5] - Tree Traversals Postorder
+        [6] - Show Tree Pre
+        [7] - Show Tree Pos
+        [8] - Search an element in Binary Tree
+        [9] - Tree Height
         ************************************""")
     return input()
 
@@ -28,31 +30,43 @@ while True:
         print("-"*20)
         if insert_element.isdigit():
             tree.insert(int(insert_element))
-            print("Node inserted successfully!")
+            print('\033[42m' + "Node inserted successfully!" + '\033[0;0m')
             print('-'*20)
         else:
-            print("Type a valid number")
+            print('\033[41m' + "Type a valid number" + '\033[0;0m')
 
     elif answer == "2":
+        print("-"*20)
         element = input("Type an element: ")
+        print("-"*20)
         try:
             tree.remove(int(element))
+            print('\033[42m' + "Node removed successfully!" + '\033[0;0m')
+            print('-'*20)
         except ValueError as e:
-            print("Type a valid number")
-    
-    elif answer == "3":
-        print("*"*20 + " Tree Pre Order "  + "*"*20)
-        tree.showTreePre(tree.getRoot(), tree.getRoot())
+            print('\033[41m' + "Type a valid number" + '\033[0;0m')
 
+    elif answer == "3":
+        print("*"*20 + '\033[43m' + " Preorder Traversal " + '\033[0;0m' + "*"*20)
+        tree.showPreOrderTraversal(tree.getRoot())
+    
     elif answer == "4":
-        print("*"*20 + " Tree InOrder "  + "*"*20)
-        tree.showTreeInOrder(tree.getRoot())
+        print("*"*20 + '\033[43m' + " Inorder Traversal "  + '\033[0;0m' + "*"*20)
+        tree.showInOrderTraversal(tree.getRoot())
 
     elif answer == "5":
-        print("*"*20 + " Tree Pos Order "  + "*"*20)
-        tree.showTreePos(tree.getRoot())
+        print("*"*20 + '\033[43m' " Postorder Traversal "  + '\033[0;0m' + "*"*20)
+        tree.showPostOrderTraversal(tree.getRoot())
 
     elif answer == "6":
+        print("*"*20 + '\033[44m' + " Tree Pre Order " + '\033[0;0m' + "*"*20)
+        tree.showTreePre(tree.getRoot(), tree.getRoot())
+
+    elif answer == "7":
+        print("*"*20 + '\033[44m' + " Tree Pos Order " + '\033[0;0m' + "*"*20)
+        tree.showTreePos(tree.getRoot())
+
+    elif answer == "8":
         print("-"*20)
         search_element = input("Type an element: ")
         print("-"*20)
@@ -64,9 +78,9 @@ while True:
                 print(f"Found node address: {node}")
                 print("-"*20)
         else:
-            print("Type a valid number")
+            print('\033[41m' + "Type a valid number" + '\033[0;0m')
 
-    elif answer == "7":
+    elif answer == "9":
         print("Tree Height: ", tree.altura(tree.getRoot()))
 
     else:
